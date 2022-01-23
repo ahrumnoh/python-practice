de573045a6905d0d9f44e8cbfcfb47b9229c7fad
ahrum noh <sydneyroseenglish@gmail.com>
2022-01-23 23:09:53 +1100

    up

--- leetcodePractice/leetcode.py
+++ leetcodePractice/leetcode.py
@@ -1,4 +1,4 @@
-# ✍️ 1. Two Sum
+# ✍️ 1. Two Sum  (EASY PART)
 
 # Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 
@@ -13,19 +13,47 @@
 # Input: nums = [2,7,11,15], target = 9
 # Output: [0,1]
 # Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
-# Example 2:
 
-# Input: nums = [3,2,4], target = 6
-# Output: [1,2]
-# Example 3:
 
-# Input: nums = [3,3], target = 6
-# Output: [0,1]
- 
 
-# Constraints:
+# for function으로 활용하기
+# class Solution:
+#     def twoSum(self, nums, target):
 
-# 2 <= nums.length <= 104
-# -109 <= nums[i] <= 109
-# -109 <= target <= 109
-# Only one valid answer exists.
+#         for i in range(len(nums)):
+#             for j in range(i+1, len(nums)):
+#                 sum = nums[i] + nums[j]
+
+#                 if sum == target:
+#                     return [i,j]
+
+
+
+
+
+
+# class Solution:
+#     def twoSum(self, nums, target):
+
+#         for i in range(len(nums)):
+#             for j in range(i+1, len(nums)):
+#                 sum = nums[i] + nums [j]
+
+#                 if sum == target:
+#                      return [i,j]
+
+
+
+
+# class Solution:
+#     def twoSum(self, nums, target):
+        
+#         for i in range(len(nums)):
+#             for j in range(i+1, len(nums)):
+#                 sum = nums[i] + nums[j]
+#                 if sum == target:
+#                     return [ i, j]
+
+
+
+
