251dbd89c5a994d80448a4fd9ae278064162c343
ahrum noh <sydneyroseenglish@gmail.com>
2022-01-26 10:25:24 +1100

    morning practice

--- practice2.py
+++ practice2.py
@@ -17,4 +17,44 @@
 
 # practice 2
 
+# print(3 * 3,  9)
+# print(3*3 ==9)
 
+
+# cal = 2 * 3 * 4
+# print(cal)
+
+# print(cal * 3)
+
+
+# name = "rose"
+# age = 27
+# salary = 150000
+# bonus = 200000
+# print(name + " salary is " + str(salary + bonus))
+
+
+# print(abs(9))  # absolute value
+
+# print(pow(4, 2))  # 16 pow =제곱
+# print(max(5, 12, 10,3))
+# print(min(2, 6, 0))
+# print(round(3.2))
+# print(round(8.9))
+
+
+# from math import *
+# print(floor(4.99))
+# print(ceil(4.8))
+# print(sqrt(9))
+
+
+from random import *
+
+# print(random()) #1미만의 임의값
+# print(random()*10)
+# print(random()+1)
+# print(int(random() * 10)+1) #integer 정수
+# # print(randint(1,45))
+# print(randrange(1, 46))
+# print("오프라인은 " + "매월" + str(randrange(1, 13)) + "  입니다.")
